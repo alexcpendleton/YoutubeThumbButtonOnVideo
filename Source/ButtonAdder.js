d6cf4c64baa8a936a98c3f9e9d8e280f07851d59
Alex Pendleton <alexcpendleton@gmail.com>
2016-03-10 20:23:10 -0500

    Clicking works now
    Still need styling, and to figure out and mimic the state of the button

--- Source/ButtonAdder.js
+++ Source/ButtonAdder.js
@@ -12,14 +12,15 @@ thumbButton.id = buttonID;
 thumbButton.className = "ytp-button";
 // Temporary Attribution: thumbs down by useiconic.com from the Noun Project https://thenounproject.com/search/?q=thumb&i=45357
 thumbButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1" preserveAspectRatio="xMidYMin" data-icon="thumb-down"  data-container-transform="scale(1 1 ) translate(1 1 )" viewBox="0 0 16 20" fill="#FFFFFF"><path  d="M0 0v8h3v-8h-3zm4 0v8c.6 0 1.006.194 1.406.594.4.4 2.294 4.219 2.594 4.719.3.5.813.787 1.313.688.5-.2.787-.713.688-1.313-.2-.5-1-3.188-1-3.688s.4-1 1-1h3c.6 0 1-.4 1-1l-2.094-6.406c-.1-.3-.506-.594-.906-.594h-7z" /></svg>';
+// Flips the thumb button: transform="scale(1,-1) translate(0, -15)"
 
 thumbButton.addEventListener("click", function() {
   console.log("on-video thumb button clicked");
   // Press the actual "Like" button
-  var actualLikeButton = findLikeButton();
-  console.log("actual like button", actualLikeButton);
+  findLikeButton().click();
+  //console.log("actual like button", actualLikeButton);
   // Toggle the state of this one to correspond with that one
-  actualLikeButton.click();
+  //actualLikeButton.click();
 });
 
 function findLikeButton() {
