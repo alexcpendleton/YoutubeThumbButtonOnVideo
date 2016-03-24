ebac92fbef7f4fea22c04e4a35c529f64ac82c3b
Alex Pendleton <alexcpendleton@gmail.com>
2016-03-23 21:35:52 -0400

    Doesn't really work, but I need a progress commit

--- Source/ButtonAdder.js
+++ Source/ButtonAdder.js
@@ -1,10 +1,57 @@
-var prefix = "yttbov-"
+var prefix = "yttbov-";
 var buttonID = prefix + "thumb";
+var classes = {
+  liked: "liked", notliked:"notliked",
+};
+
+var nodeFinder = {
+  likeButtonSection: function() {
+    return document.querySelector(".like-button-renderer");
+  },
+  likedButton: function(parent) {
+    return parent.querySelector(".like-button-clicked");
+  },
+  notLikedButton: function(parent) {
+    return parent.querySelector(".like-button-unclicked");
+  },
+  isLiked: function(activeThumbButton) {
+    return activeThumbButton.matches(".like-button-clicked");
+  }
+};
+
+var styler = {
+  applyLikedStyle: function(applyTo) {},
+  applyNotLikedStyle: function(applyTo) {}
+};
+
+var cssClassStyler = {
+  applyLikedStyle: function(applyTo) {
+    applyTo.classList.remove(classes.notLiked);
+    applyTo.classList.add(classes.liked);
+  },
+  applyNotLikedStyle: function(applyTo) {
+    applyTo.classList.remove(classes.liked);
+    applyTo.classList.add(classes.notLiked);
+  }
+};
+
+var styleAttributeStyler = {
+  applyLikedStyle: function(applyTo) {
+    applyTo.querySelector("svg").style = "fill:#FFFFFF";
+  },
+  applyNotLikedStyle: function(applyTo) {
+    applyTo.querySelector("svg").style = "fill:red";
+  }
+};
+
+styler = styleAttributeStyler;
+
 var existingButton = document.querySelector("#"+buttonID);
 if (existingButton) {
   console.log("Removing existing button");
   existingButton.parentElement.removeChild(existingButton);
 }
+var customCss = "#" +buttonID +".unliked svg { fill: red; } "
 var targetContainer = document.querySelector(".ytp-right-controls");
 var thumbButton = document.createElement("button");
 thumbButton.name = buttonID;
@@ -18,17 +65,66 @@ thumbButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="100%" w
 thumbButton.addEventListener("click", function() {
   console.log("on-video thumb button clicked");
   // Press the actual "Like" button
-  findLikeButton().click();
+  findActiveThumbButton().click();
   //console.log("actual like button", actualLikeButton);
   // Toggle the state of this one to correspond with that one
   //actualLikeButton.click();
 });
 
-function findLikeButton() {
+
+function deriveLikeButtonStatus(activeThumbButton) {
+  return nodeFinder.isLiked(activeThumbButton);
+}
+
+function observeExistingLikeButtonSection(mutations) {
+  // signal mutation
+  var activeThumbButton = findActiveThumbButton();
+  var mutationSettings = {
+    childList: false, subtree: false, attributes:true, characterData:false,
+    attributeOldValue: true, attributeFilter: "class"
+  };
+  var likedButton = nodeFinder.likedButton();
+  var notLikedButton = nodeFinder.notLikedButton();
+
+  function hidingClassChanged(before, after) {
+    var hidingClass = "hid";
+    return !(before.contains(hidingClass) && after.contains(hidingClass));
+  }
+  function hidingClassMutated(mutations, observer) {
+    var newValue = observer.target.classList;
+    foreach(var m in mutations) {
+      if (m.type === "attributes" && m.attributeName === "class") {
+        return hidingClassChanged(m.oldValue)
+      }
+    }
+  }
+
+  var observers = {};
+  observers.liked = new MutationObserver();
+  observers.notLiked = new MutationObserver();
+
+  var isLiked = nodeFinder.isLiked();
+
+  if (isLiked) {
+    styler.applyLikedStyle(activeThumbButton);
+  } else {
+    styler.applyNotLikedStyle(activeThumbButton);
+  }
+}
+
+var observer = new MutationObserver(observeExistingLikeButtonSection);
+var likeButtonSection = nodeFinder.likeButtonSection();
+if (likeButtonSection) {
+  console.log("observing: ", likeButtonSection);
+  observer.observe(likeButtonSection, );
+}
+
+function findActiveThumbButton() {
   /* There are two buttons on the page for liking/unliking. The one
   without the .hid class is the one that should be clicked. */
   var selector = ".like-button-renderer-like-button:not(.hid)";
   var youtubeLikeButton = document.querySelector(selector);
+  console.log("found active button:", youtubeLikeButton);
   return youtubeLikeButton;
 }
 
